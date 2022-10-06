7703add38ce2e6292e0bbbbe5f43000477751363
AK <akrivit@gmail.com>
2022-10-06 01:16:04 -0400

    toMatchObject extension; button tracking
    painful hours trying to express 'array contains object which matches'

--- tests/e2e.test.js
+++ tests/e2e.test.js
@@ -1,25 +1,29 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 require('dotenv').config();
 const { PROJECT, TOKEN, SECRET, ACCT, PASS, } = process.env;
-const eventPayloads = [];
-const profilePayloads = [];
-const TRACK = `https://api-js.mixpanel.com/track/`
-const ENGAGE = `https://api-js.mixpanel.com/engage/`
+const eventStream = [];
+const profileStream = [];
+const TRACK = `https://api-js.mixpanel.com/track/`;
+const ENGAGE = `https://api-js.mixpanel.com/engage/`;
 
+// oy.... https://lightrun.com/answers/facebook-jest-expose-matchers-in-expectextend
+const { getMatchers } = require("../node_modules/expect/build/jestMatchersObject");
+
+// DOCS:
 // https://github.com/smooth-code/jest-puppeteer#api
+// https://jestjs.io/docs/expect
+
 beforeAll(async () => {
 	await page.setRequestInterception(true);
 	page.on('request', interceptedRequest => {
 		if (interceptedRequest.url().includes(TRACK)) {
 			const eventData = JSON.parse(decodeURIComponent(interceptedRequest.postData()).split('data=').slice(-1)[0]);
-			eventPayloads.push(eventData);
+			eventStream.push(eventData);
 			interceptedRequest.continue();
 
 		}
 		else if (interceptedRequest.url().includes(ENGAGE)) {
 			const profileData = JSON.parse(decodeURIComponent(interceptedRequest.postData()).split('data=').slice(-1)[0]);
-			profilePayloads.push(profileData);
+			profileStream.push(profileData);
 			interceptedRequest.continue();
 
 		}
@@ -27,7 +31,7 @@ beforeAll(async () => {
 			interceptedRequest.continue();
 		}
 	});
-	
+
 	await page.goto(`http://localhost:5000?token=${TOKEN}`);
 });
 
@@ -42,31 +46,65 @@ describe('loading the tag', () => {
 		//page will throw if the browser throws
 		await expect(page.title()).resolves.toMatch('mpEZTrack E2E test');
 	});
+
+	test('valid token bootstraps tracking', async () => {
+		let spec = {
+			properties: {
+				token: TOKEN
+			}
+		};
+		expect(eventStream.flat()).toContainObjectMatching(spec);
+	});
 });
 
 describe('compenents track properly', () => {
 
 	test('page view tracking', async () => {
-		const pageView = eventPayloads.flat().pop();
-		expect(pageView).toMatchObject({ event: 'page enter', properties: { token: TOKEN } });
+		let spec = {
+			event: 'page enter',
+			properties: {
+				token: TOKEN
+			}
+		};
+		expect(eventStream.flat()).toContainObjectMatching(spec);
 
 	});
 
 	test('buttons tracking', async () => {
 		await page.click('#normalButton');
-		await page.waitForTimeout(100);
-		const normalButton = eventPayloads.flat().pop();
-		expect(normalButton).toMatchObject({ event: 'button click', properties: { token: TOKEN, "BUTTON → id": "normalButton" } });
+		await sleep();
+		let normalSpec = { event: 'button click', properties: { "BUTTON → id": "normalButton" } };
+		expect(eventStream.flat()).toContainObjectMatching(normalSpec);
 
 		await page.click('#inputButton');
-		await page.waitForTimeout(100);
-		const inputButton = eventPayloads.flat().pop();
-		expect(inputButton).toMatchObject({ event: 'button click', properties: { token: TOKEN, "BUTTON → id": "inputButton" } });
+		await sleep();
+		let inputSpec = { event: 'button click', properties: { "BUTTON → id": "inputButton" } };
+		expect(eventStream.flat()).toContainObjectMatching(inputSpec);
 
 		await page.click('#divButton');
-		await page.waitForTimeout(100);
-		const divButton = eventPayloads.flat().pop();
-		expect(divButton).toMatchObject({ event: 'button click', properties: { token: TOKEN, "BUTTON → id": "divButton" } });
+		await sleep();
+		let divSpec = { event: 'button click', properties: { "BUTTON → id": "divButton" } };
+		expect(eventStream.flat()).toContainObjectMatching(divSpec);
+
+		await page.click('#filePicker');
+		await sleep();
+		let filePickerSpec = { event: 'button click', properties: { "BUTTON → id": "filePicker" } };
+		expect(eventStream.flat()).toContainObjectMatching(filePickerSpec);
+
+		await page.click('#imageButton');
+		await sleep();
+		let imageButtonSpec = { event: 'button click', properties: { "BUTTON → id": "imageButton" } };
+		expect(eventStream.flat()).toContainObjectMatching(imageButtonSpec);
+
+		await page.click('#resetButton');
+		await sleep();
+		let resetSpec = { event: 'button click', properties: { "BUTTON → id": "resetButton" } };
+		expect(eventStream.flat()).toContainObjectMatching(resetSpec);
+
+		await page.click('#submitButton');
+		await sleep();
+		let submitButtonSpec = { event: 'button click', properties: { "BUTTON → id": "submitButton" } };
+		expect(eventStream.flat()).toContainObjectMatching(submitButtonSpec);
 	});
 });
 
@@ -80,3 +118,42 @@ describe('bad token throws', () => {
 	});
 });
 
+
+async function sleep(ms = 40) {
+	await page.waitForTimeout(ms);
+}
+
+/**
+ *  UTILITY: toContainObjectMatching(rec, exp)
+ *	Deep-match an array of objects against another object.
+ *	runs expect().toMatchObject() in a loop; passes if any object in recieved array matches
+ *	requires grabbing the internals of expect()
+ *	https://github.com/jest-community/jest-extended/issues/146#issuecomment-1212283459	 
+
+**/
+expect.extend({	
+	toContainObjectMatching(received, expected) {
+		let matchFound = false;
+		if (Array.isArray(received)) {
+			matchFound =
+				received.map((obj) => {
+					const result = getMatchers().toMatchObject.call(this, obj, expected);
+					// For some reason TSC doesn't fully recognize result as object of type
+					// ExpectationResult
+					return (result).pass;
+				}).reduce((a, b) => a || b, false);
+		}
+
+		const prettyPrinted = {
+			received: this.utils.printReceived(received),
+			expected: this.utils.printExpected(expected),
+		};
+		return {
+			message: () =>
+				this.isNot
+					? `expected ${prettyPrinted.received} to be list containing no object matching ${prettyPrinted.expected}`
+					: `expected ${prettyPrinted.received} to be list containing at least one object matching ${prettyPrinted.expected}`,
+			pass: matchFound,
+		};
+	},
+});
